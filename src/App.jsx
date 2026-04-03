232bd825e18547ec094b29a208115d366aaae807
Amine Sekhal <Aminesorati35@gmail.com>
2026-04-03 23:38:00 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ import DownloadPage from "./pages/DownloadPage";
 import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
-  
+  const [isTikTokBrowser, setIsTikTokBrowser] = useState(null);
+
+  useEffect(() => {
+    const ua = navigator.userAgent.toLowerCase();
+
+    const isTikTok =
+      ua.includes("tiktok") ||
+      ua.includes("bytedance") ||
+      ua.includes("musical_ly") ||
+      ua.includes("aweme");
+
+    setIsTikTokBrowser(isTikTok);
+  }, []);
+
+  if (isTikTokBrowser) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black">
+        <img
+          src="/images/browser/5.gif"
+          alt="Open in browser"
+          className="w-[300px] max-w-[90%]"
+        />
+      </div>
+    );
+  }
 
   // ✅ الموقع عادي
   return (
