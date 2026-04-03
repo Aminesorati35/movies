4a0b5042486c364e354ad15290da2a88a858c641
Amine Sekhal <Aminesorati35@gmail.com>
2026-04-03 23:31:21 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,7 @@ import ScrollToTop from "./components/ScrollToTop";
 export default function App() {
   const [isTikTokBrowser, setIsTikTokBrowser] = useState(null);
 
-  useEffect(() => {
-    const ua = navigator.userAgent.toLowerCase();
 
-    const isTikTok =
-      ua.includes("tiktok") ||
-      ua.includes("bytedance") ||
-      ua.includes("musical_ly") ||
-      ua.includes("aweme");
-
-    setIsTikTokBrowser(isTikTok);
-  }, []);
-
-  if (isTikTokBrowser) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-black">
-        <img
-          src="/images/browser/5.gif"
-          alt="Open in browser"
-          className="w-[300px] max-w-[90%]"
-        />
-      </div>
-    );
-  }
 
   // ✅ الموقع عادي
   return (
